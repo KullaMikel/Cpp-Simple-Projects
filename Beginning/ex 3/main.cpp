28ca6d62e1339e692ad8d215c712e8f1a7d3885d
Mikel Kulla <104671003+KullaMikel@users.noreply.github.com>
2022-05-02 10:17:34 +0200

    new excercise
    Simple math excersize

---
+++ Beginning/ex 3/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float x,y,f;
+    cout<<"Input x and y:"<<endl;
+    cin>>x>>y;
+    f=((x*x)+(y*y))/(((x*x)+2)*y);
+    if(y>0 )
+    {
+        cout<<"Value of function= "<<f;
+    }
+    else if(y<=0)
+    {
+        cout << "No solution!"<<endl;
+    }
+    return 0;
+}
